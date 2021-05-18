cd9554bedc60396388e67372c1c96264b7e82fc5
João Victor Ribeiro Ferro <39158742+joaovictorferro@users.noreply.github.com>
2021-05-18 17:29:21 -0300

    att

--- Analisador_Lexico/src/main/MainClass.java
+++ Analisador_Lexico/src/main/MainClass.java
@@ -8,20 +8,4 @@ import src.lexico.Token;
 public class MainClass {
 	public static void main (String[]args) {
 		Sintatico analiSintatico = new Sintatico("input.jvlal");
-//		try {
-//			JLScanner sc = new JLScanner("input.jvlal");
-//			Token token;
-//			do {
-//				token = sc.nextToken();
-//				if(token != null) {
-//					System.out.println(token);
-//				}
-//			}while(token != null);
-//		} catch(JLLexicalException ex) {
-//			System.out.println("Lexical ERROR " + ex.getMessage());
-//		}catch(Exception ex) {
-//			System.out.println(ex);
-//		}
-	}
-
 }
